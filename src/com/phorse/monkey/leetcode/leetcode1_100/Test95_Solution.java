d0e29c635a66863a495ee65bfb29c5b300f429e4
luoxusheng <luoxusheng@xiaomi.com>
2021-02-26 18:52:22 +0800

    ✨ Feat: leetCode95 不同的二叉搜索树 II

---
+++ src/com/phorse/monkey/leetcode/leetcode1_100/Test95_Solution.java
@@ -0,0 +1,77 @@
+package com.phorse.monkey.leetcode.leetcode1_100;
+
+import com.phorse.monkey.leetcode.data.TreeNode;
+import com.phorse.monkey.utils.TreeUtils;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 给定一个整数 n，生成所有由 1 ... n 为节点所组成的 二叉搜索树 。
+ *
+ * 示例：
+ *
+ * 输入：3
+ * 输出：
+ * [
+ *   [1,null,3,2],
+ *   [3,2,null,1],
+ *   [3,1,null,null,2],
+ *   [2,1,3],
+ *   [1,null,2,null,3]
+ * ]
+ * 解释：
+ * 以上的输出对应以下 5 种不同结构的二叉搜索树：
+ *
+ *    1         3     3      2      1
+ *     \       /     /      / \      \
+ *      3     2     1      1   3      2
+ *     /     /       \                 \
+ *    2     1         2                 3
+ *
+ * @author luoxusheng 2021/2/26 18:34
+ * @version v1.0.0
+ */
+public class Test95_Solution {
+
+    public static List<TreeNode> generateTrees(int n) {
+        if (n == 0) {
+            return new LinkedList<>();
+        }
+        return generateTrees(1, n);
+    }
+
+    public static List<TreeNode> generateTrees(int start, int end) {
+        List<TreeNode> allTrees = new LinkedList<>();
+        if (start > end) {
+            allTrees.add(null);
+            return allTrees;
+        }
+
+        // 枚举可行根节点
+        for (int i = start; i <= end; i++) {
+            // 获得所有可行的左子树集合
+            List<TreeNode> leftTrees = generateTrees(start, i - 1);
+
+            // 获得所有可行的右子树集合
+            List<TreeNode> rightTrees = generateTrees(i + 1, end);
+
+            // 从左子树集合中选出一棵左子树，从右子树集合中选出一棵右子树，拼接到根节点上
+            for (TreeNode left : leftTrees) {
+                for (TreeNode right : rightTrees) {
+                    TreeNode currTree = new TreeNode(i);
+                    currTree.left = left;
+                    currTree.right = right;
+                    allTrees.add(currTree);
+                }
+            }
+        }
+        return allTrees;
+    }
+
+    public static void main(String[] args) {
+        List<TreeNode> treeNodes = generateTrees(5);
+        treeNodes.forEach(TreeUtils::show);
+    }
+}
---
+++ src/com/phorse/monkey/utils/TreeUtils.java
@@ -0,0 +1,7 @@
+package com.phorse.monkey.utils;/**
+ * TODO
+ * @author luoxusheng 2021/2/26 18:45
+ * @version v1.0.0
+ */
+public class TreeUtils {
+}
