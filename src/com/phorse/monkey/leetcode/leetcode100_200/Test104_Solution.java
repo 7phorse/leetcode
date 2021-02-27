c7457a4a3ea61b431ba82c1cc7094155a51290f3
luoxusheng <luoxusheng@xiaomi.com>
2021-02-27 14:03:09 +0800

    ✨ Feat: leetCode104 二叉树的最大深度

--- src/com/phorse/monkey/leetcode/leetcode100_200/Test104_Solution.java
+++ src/com/phorse/monkey/leetcode/leetcode100_200/Test104_Solution.java
@@ -10,7 +10,7 @@ import com.phorse.monkey.leetcode.data.TreeNode;
  *
  * 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
  *
- * 说明: 叶子节点是指没有子节点的节点。
+ * 说明:叶子节点是指没有子节点的节点。
  *
  * 示例：
  * 给定二叉树 [3,9,20,null,null,15,7]，
@@ -22,13 +22,32 @@ import com.phorse.monkey.leetcode.data.TreeNode;
  *    15   7
  * 返回它的最大深度 3 。
  *
- * 来源：力扣（LeetCode）
- * 链接：https://leetcode-cn.com/problems/maximum-depth-of-binary-tree
- * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class Test104_Solution {
 
+    /**
+     * 思路：基础递归算法
+     * 1. 定义递归终止条件， 当root==null时，递归结束
+     * 2. 分别计算左右子树的最大深度
+     * 3. 取最大值，加上当前根节点（+1）为最大深度
+     *
+     * @author luoxusheng 2021/2/27 13:58
+     */
     public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftNum = maxDepth(root.left);
+        int rightNum = maxDepth(root.right);
+        return Math.max(leftNum, rightNum) + 1;
+    }
+
+    /**
+     * 思路：基础递归算法，将深度index作为变量向下传递
+     *
+     * @author luoxusheng 2020/8/7 13:56
+     */
+    public int maxDepth1(TreeNode root) {
         return maxDepth(0, root);
     }
 
