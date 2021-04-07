22257c18b0aac8e35668a4a188ce2b1d2a072c67
luoxusheng <luoxusheng@xiaomi.com>
2021-04-07 19:51:54 +0800

    ✨ Feat: leetCode33 搜索旋转排序数组

---
+++ src/com/phorse/monkey/leetcode/leetcode1_100/Test33_Solution.java
@@ -0,0 +1,77 @@
+package com.phorse.monkey.leetcode.leetcode1_100;
+
+/**
+ * 整数数组 nums 按升序排列，数组中的值 互不相同 。
+ * <p>
+ * 在传递给函数之前，nums 在预先未知的某个下标 k（0 <= k < nums.length）上进行了 旋转，使数组变为 [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]]（下标 从 0 开始 计数）。例如， [0,1,2,4,5,6,7] 在下标 3 处经旋转后可能变为[4,5,6,7,0,1,2] 。
+ * <p>
+ * 给你 旋转后 的数组 nums 和一个整数 target ，如果 nums 中存在这个目标值 target ，则返回它的下标，否则返回-1。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [4,5,6,7,0,1,2], target = 0
+ * 输出：4
+ * 示例2：
+ * <p>
+ * 输入：nums = [4,5,6,7,0,1,2], target = 3
+ * 输出：-1
+ * 示例 3：
+ * <p>
+ * 输入：nums = [1], target = 0
+ * 输出：-1
+ *
+ * @author luoxusheng 2021/4/7 13:47
+ * @version v1.0.0
+ */
+public class Test33_Solution {
+
+    /**
+     * 思路：二分查找
+     */
+    public static int search1(int[] nums, int target) {
+        int n = nums.length;
+        if (n == 0) {
+            return -1;
+        }
+        if (n == 1) {
+            return nums[0] == target ? 0 : -1;
+        }
+        int l = 0, r = n - 1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[0] <= nums[mid]) {
+                if (nums[0] <= target && target < nums[mid]) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[n - 1]) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+
+    public int search(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(search1(new int[]{4, 5, 6, 7, 0, 1, 2}, 0));
+    }
+}
